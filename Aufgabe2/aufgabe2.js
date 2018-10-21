1271e324328842ae521557d7a0ae0ffa7c5a6a8f
Jana <Jana@ardians-mbp.hfu-open.fu.hs-furtwangen.de>
2018-10-21 16:31:04 +0200

    Aufgabe 2

--- Aufgabe2/aufgabe2.js
+++ Aufgabe2/aufgabe2.js
@@ -1,7 +1,105 @@
 var uno;
 (function (uno) {
+    var farben = ["blue", "red", "yellow", "green"];
+    var aktionsKarten = ["+2", "<>", "A"]; // 8 stk / 2 pro Farbe
+    var jokerKarten = ["FW", "+4"]; // 4 pro Joker
+    var zahlenKarten = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var kartendeck;
+    var meineKarten;
+    var Karte = (function () {
+        function Karte(_farbe, _art) {
+            this.farbe = _farbe;
+            this.art = _art;
+        }
+        return Karte;
+    }());
     function start() {
-        var str;
+        var str = prompt("Bitte die Spielkartenanzahl auswählen: ", "5");
+        var anzahl = Number(str);
+        erzeugeAlleKarten();
+        meineKarten = [];
+        for (var i = 0; i < anzahl; i++) {
+            zufallskarteZiehen();
+        }
+        meineKartenZeichnen();
     }
+    function erzeugeAlleKarten() {
+        kartendeck = [];
+        for (var i = 0; i < farben.length; i++) {
+            var farbe = farben[i];
+            // Aktionskarten
+            for (var j = 0; j < 2; j++) {
+                for (var k = 0; k < aktionsKarten.length; k++) {
+                    var art = aktionsKarten[k];
+                    var karte = new Karte(farbe, art);
+                    kartendeck.push(karte);
+                }
+            }
+            // Zahlenkarten
+            for (j = 0; j < 4; j++) {
+                for (k = 0; k < zahlenKarten.length; k++) {
+                    art = zahlenKarten[k];
+                    karte = new Karte(farbe, art);
+                    kartendeck.push(karte);
+                }
+            }
+        }
+        // Jokerkarten
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < jokerKarten.length; j++) {
+                art = jokerKarten[j];
+                karte = new Karte(farbe, art);
+                kartendeck.push(karte);
+            }
+        }
+    }
+    function zufallskarteZiehen() {
+        console.log("Hello");
+        // Anzahl verbleibender Karten im Kartendeck
+        var anzahlVerbleibenderKartenImStapel = kartendeck.length;
+        // Zufälligen Index erzeugen, welche Karte wir aus dem kartendeck nehmen
+        var index = Math.floor(Math.random() * anzahlVerbleibenderKartenImStapel);
+        // Karte aus dem Kartendeck holen
+        var karte = kartendeck[index];
+        // Karte aus dem Deck entfernen, damit diese nicht doppelt gezogen werden kann
+        kartendeck.splice(index, 1);
+        meineKarten.push(karte);
+    }
+    function meineKartenZeichnen() {
+        for (var i = 0; i < meineKarten.length; i++) {
+            var karte = meineKarten[i];
+            //<div class="card"> 
+            var card = document.createElement("div");
+            card.className = "card";
+            // <div class="bg green"></div> 
+            var bg = document.createElement("div");
+            bg.className = "bg " + karte.farbe;
+            card.appendChild(bg);
+            // <div class="circle"></div> 
+            var kreis = document.createElement("div");
+            kreis.className = "circle";
+            card.appendChild(kreis);
+            //  <div class="top-left">9</div> 
+            var obenLinks = document.createElement("div");
+            obenLinks.className = "top-left";
+            obenLinks.innerHTML = karte.art;
+            card.appendChild(obenLinks);
+            //  <div class="center">9</div> 
+            var zentrum = document.createElement("div");
+            zentrum.className = "center";
+            zentrum.innerHTML = karte.art;
+            card.appendChild(zentrum);
+            // <div class="bottom-right">9</div> 
+            var untenRechts = document.createElement("div");
+            untenRechts.className = "bottom-right";
+            untenRechts.innerHTML = karte.art;
+            card.appendChild(untenRechts);
+            var style = card.style;
+            style.left = 10 + i * 100 + "px";
+            style.bottom = "10px";
+            document.body.appendChild(card);
+        }
+    }
+    document.addEventListener("DOMContentLoaded", start);
 })(uno || (uno = {}));
 //# sourceMappingURL=aufgabe2.js.map
--- Aufgabe2/aufgabe2.ts
+++ Aufgabe2/aufgabe2.ts
@@ -1,5 +1,132 @@
 namespace uno {
-    
+
+    let farben: string[] = ["blue", "red", "yellow", "green"];
+    let aktionsKarten: string[] = ["+2", "<>", "A"]; // 8 stk / 2 pro Farbe
+    let jokerKarten: string[] = ["FW", "+4"]; // 4 pro Joker
+    let zahlenKarten: string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var kartendeck: Karte[];
+    var meineKarten: Karte[];
+
+    class Karte {
+        farbe: string;
+        art: string;
+
+        constructor( _farbe: string, _art: string ) {
+            this.farbe = _farbe;
+            this.art = _art;
+        }
+    }
+
     function start(): void {
-        var str: string}
+        var str: string = prompt( "Bitte die Spielkartenanzahl auswählen: ", "5" );
+        var anzahl: number = Number( str );
+
+        erzeugeAlleKarten();
+        meineKarten = [];
+
+        for ( var i: number = 0; i < anzahl; i++ ) {
+            zufallskarteZiehen();
+        }
+
+        meineKartenZeichnen();
+    }
+
+    function erzeugeAlleKarten(): void {
+        kartendeck = [];
+        for ( var i: number = 0; i < farben.length; i++ ) {
+            var farbe: string = farben[i];
+
+            // Aktionskarten
+            for ( var j: number = 0; j < 2; j++ ) {
+                for ( var k: number = 0; k < aktionsKarten.length; k++ ) {
+                    var art: string = aktionsKarten[k];
+                    var karte: Karte = new Karte( farbe, art );
+                    kartendeck.push( karte );
+                }
+            }
+
+            // Zahlenkarten
+            for ( j = 0; j < 4; j++ ) {
+                for ( k = 0; k < zahlenKarten.length; k++ ) {
+                    art = zahlenKarten[k];
+                    karte = new Karte( farbe, art );
+                    kartendeck.push( karte );
+                }
+            }
+        }
+
+        // Jokerkarten
+        for ( i = 0; i < 4; i++ ) {
+            for ( j = 0; j < jokerKarten.length; j++ ) {
+                art = jokerKarten[j];
+                karte = new Karte( farbe, art );
+                kartendeck.push( karte );
+            }
+        }
+    }
+
+    function zufallskarteZiehen(): void {
+        console.log( "Hello" );
+
+        // Anzahl verbleibender Karten im Kartendeck
+        var anzahlVerbleibenderKartenImStapel: number = kartendeck.length;
+
+        // Zufälligen Index erzeugen, welche Karte wir aus dem kartendeck nehmen
+        var index: number = Math.floor( Math.random() * anzahlVerbleibenderKartenImStapel );
+
+        // Karte aus dem Kartendeck holen
+        var karte: Karte = kartendeck[index];
+
+        // Karte aus dem Deck entfernen, damit diese nicht doppelt gezogen werden kann
+        kartendeck.splice( index, 1 );
+
+        meineKarten.push( karte );
+    }
+
+    function meineKartenZeichnen(): void {
+
+        for ( var i: number = 0; i < meineKarten.length; i++ ) {
+            var karte: Karte = meineKarten[i];
+
+            //<div class="card"> 
+            let card: HTMLDivElement = document.createElement( "div" );
+            card.className = "card";
+
+            // <div class="bg green"></div> 
+            let bg: HTMLDivElement = document.createElement( "div" );
+            bg.className = "bg " + karte.farbe;
+            card.appendChild( bg );
+
+            // <div class="circle"></div> 
+            let kreis: HTMLDivElement = document.createElement( "div" );
+            kreis.className = "circle";
+            card.appendChild( kreis );
+
+            //  <div class="top-left">9</div> 
+            let obenLinks: HTMLDivElement = document.createElement( "div" );
+            obenLinks.className = "top-left";
+            obenLinks.innerHTML = karte.art;
+            card.appendChild( obenLinks );
+
+            //  <div class="center">9</div> 
+            let zentrum: HTMLDivElement = document.createElement( "div" );
+            zentrum.className = "center";
+            zentrum.innerHTML = karte.art;
+            card.appendChild( zentrum );
+
+            // <div class="bottom-right">9</div> 
+            let untenRechts: HTMLDivElement = document.createElement( "div" );
+            untenRechts.className = "bottom-right";
+            untenRechts.innerHTML = karte.art;
+            card.appendChild( untenRechts );
+
+            let style: CSSStyleDeclaration = card.style;
+            style.left = 10 + i * 100 + "px";
+            style.bottom = "10px";
+
+            document.body.appendChild( card );
+        }
+    }
+
+    document.addEventListener( "DOMContentLoaded", start );
 }
