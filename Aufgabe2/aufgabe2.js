ff0c89dd64404bddd7b2a28ff19b6fb4f3ecf189
Jana <Jana@ardians-mbp.hfu-open.fu.hs-furtwangen.de>
2018-10-24 23:18:18 +0200

    Überarbeitet

--- Aufgabe2/aufgabe2.js
+++ Aufgabe2/aufgabe2.js
@@ -1,25 +1,29 @@
 var uno;
 (function (uno) {
-    var farben = ["blue", "red", "yellow", "green"];
+    var farben = ["blue", "red", "yellow", "green"]; //alle Farben die wir haben, werden festgelegt
     var aktionsKarten = ["+2", "<>", "A"]; // 8 stk / 2 pro Farbe
     var jokerKarten = ["FW", "+4"]; // 4 pro Joker
     var zahlenKarten = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    //die restlichen Spielkarten
     var kartendeck;
+    //meine eigenen Karten auf der Hand
     var meineKarten;
+    //eine Klasse, damit werden die Karten beschrieben, Farbe und Zahl oder Zeichen
     var Karte = (function () {
         function Karte(_farbe, _art) {
-            this.farbe = _farbe;
+            this.farbe = _farbe; //this= der Zugriff auf die Farbe und Art bzw Zeichen
             this.art = _art;
         }
         return Karte;
     }());
+    // Spiel wird initialisiert
     function start() {
         var str = prompt("Bitte die Spielkartenanzahl auswählen: ", "5");
         var anzahl = Number(str);
-        erzeugeAlleKarten();
+        erzeugeAlleKarten(); //Funktionsaufruf
         meineKarten = [];
         for (var i = 0; i < anzahl; i++) {
-            zufallskarteZiehen();
+            zufallskarteZiehen(); //Funktionsaufruf
         }
         meineKartenZeichnen();
     }
@@ -32,7 +36,7 @@ var uno;
                 for (var k = 0; k < aktionsKarten.length; k++) {
                     var art = aktionsKarten[k];
                     var karte = new Karte(farbe, art);
-                    kartendeck.push(karte);
+                    kartendeck.push(karte); //Karte wird auf den Stapel gelegt
                 }
             }
             // Zahlenkarten
@@ -57,38 +61,38 @@ var uno;
         console.log("Hello");
         // Anzahl verbleibender Karten im Kartendeck
         var anzahlVerbleibenderKartenImStapel = kartendeck.length;
-        // Zufälligen Index erzeugen, welche Karte wir aus dem kartendeck nehmen
+        // Zufälliger Index wird erzeugt, welche Karte aus dem Deck gezogen wird
         var index = Math.floor(Math.random() * anzahlVerbleibenderKartenImStapel);
-        // Karte aus dem Kartendeck holen
+        // Karte aus dem Deck holen
         var karte = kartendeck[index];
-        // Karte aus dem Deck entfernen, damit diese nicht doppelt gezogen werden kann
+        // Die Karte wird aus dem Deck entfernt, damit die nicht doppelt gezogen wird.
         kartendeck.splice(index, 1);
-        meineKarten.push(karte);
+        meineKarten.push(karte); // Die Karte wird auf meinen Stapel gelegt
     }
     function meineKartenZeichnen() {
         for (var i = 0; i < meineKarten.length; i++) {
             var karte = meineKarten[i];
-            //<div class="card"> 
+            //<div class="card">  und die Erzeugung von einem HTML Tags, damit die Karten angezeigt werden
             var card = document.createElement("div");
             card.className = "card";
             // <div class="bg green"></div> 
             var bg = document.createElement("div");
             bg.className = "bg " + karte.farbe;
-            card.appendChild(bg);
-            // <div class="circle"></div> 
+            card.appendChild(bg); // der Hintergrund dieser Karte wird hinzugefügt
+            // <div class="circle"></div> und Ellipse wird ebenfalls hinzugefügt
             var kreis = document.createElement("div");
             kreis.className = "circle";
-            card.appendChild(kreis);
+            card.appendChild(kreis); // Hinzufügen der Ellipse für die Optik
             //  <div class="top-left">9</div> 
             var obenLinks = document.createElement("div");
             obenLinks.className = "top-left";
-            obenLinks.innerHTML = karte.art;
-            card.appendChild(obenLinks);
+            obenLinks.innerHTML = karte.art; // Das Motiv der Karte, Zahl oder Zeichen
+            card.appendChild(obenLinks); // Die Zahl wird oben links zur Karte hinzugefügt
             //  <div class="center">9</div> 
             var zentrum = document.createElement("div");
             zentrum.className = "center";
-            zentrum.innerHTML = karte.art;
-            card.appendChild(zentrum);
+            zentrum.innerHTML = karte.art; // Das Hinzufügen der Zahl zentriert auf die Karte
+            card.appendChild(zentrum); // Die Zahl wird zentriert und hinzugefügt
             // <div class="bottom-right">9</div> 
             var untenRechts = document.createElement("div");
             untenRechts.className = "bottom-right";
@@ -97,7 +101,7 @@ var uno;
             var style = card.style;
             style.left = 10 + i * 100 + "px";
             style.bottom = "10px";
-            document.body.appendChild(card);
+            document.body.appendChild(card); // Karte wird im HTML-Body hinzugefügt
         }
     }
     document.addEventListener("DOMContentLoaded", start);
--- Aufgabe2/aufgabe2.ts
+++ Aufgabe2/aufgabe2.ts
@@ -1,52 +1,56 @@
 namespace uno {
 
-    let farben: string[] = ["blue", "red", "yellow", "green"];
+    let farben: string[] = ["blue", "red", "yellow", "green"]; //alle Farben die wir haben, werden festgelegt
     let aktionsKarten: string[] = ["+2", "<>", "A"]; // 8 stk / 2 pro Farbe
     let jokerKarten: string[] = ["FW", "+4"]; // 4 pro Joker
     let zahlenKarten: string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    
+    //die restlichen Spielkarten
     var kartendeck: Karte[];
+    //meine eigenen Karten auf der Hand
     var meineKarten: Karte[];
 
+    //eine Klasse, damit werden die Karten beschrieben, Farbe und Zahl oder Zeichen
     class Karte {
         farbe: string;
         art: string;
 
         constructor( _farbe: string, _art: string ) {
-            this.farbe = _farbe;
+            this.farbe = _farbe; //this= der Zugriff auf die Farbe und Art bzw Zeichen
             this.art = _art;
         }
     }
-
+    // Spiel wird initialisiert
     function start(): void {
         var str: string = prompt( "Bitte die Spielkartenanzahl auswählen: ", "5" );
         var anzahl: number = Number( str );
 
-        erzeugeAlleKarten();
+        erzeugeAlleKarten(); //Funktionsaufruf
         meineKarten = [];
 
         for ( var i: number = 0; i < anzahl; i++ ) {
-            zufallskarteZiehen();
+            zufallskarteZiehen(); //Funktionsaufruf
         }
 
         meineKartenZeichnen();
     }
 
-    function erzeugeAlleKarten(): void {
+    function erzeugeAlleKarten(): void { //ohne Rückgabewert
         kartendeck = [];
-        for ( var i: number = 0; i < farben.length; i++ ) {
+        for ( var i: number = 0; i < farben.length; i++ ) { //die äußere Schleife läuft 4 mal durch
             var farbe: string = farben[i];
 
             // Aktionskarten
-            for ( var j: number = 0; j < 2; j++ ) {
+            for ( var j: number = 0; j < 2; j++ ) { // nur zwei von jeder Farbe
                 for ( var k: number = 0; k < aktionsKarten.length; k++ ) {
                     var art: string = aktionsKarten[k];
                     var karte: Karte = new Karte( farbe, art );
-                    kartendeck.push( karte );
+                    kartendeck.push( karte ); //Karte wird auf den Stapel gelegt
                 }
             }
 
             // Zahlenkarten
-            for ( j = 0; j < 4; j++ ) {
+            for ( j = 0; j < 4; j++ ) { // nur vier von jeder Farbe
                 for ( k = 0; k < zahlenKarten.length; k++ ) {
                     art = zahlenKarten[k];
                     karte = new Karte( farbe, art );
@@ -65,54 +69,54 @@ namespace uno {
         }
     }
 
-    function zufallskarteZiehen(): void {
+    function zufallskarteZiehen(): void {   // Deklaration
         console.log( "Hello" );
 
         // Anzahl verbleibender Karten im Kartendeck
         var anzahlVerbleibenderKartenImStapel: number = kartendeck.length;
 
-        // Zufälligen Index erzeugen, welche Karte wir aus dem kartendeck nehmen
+        // Zufälliger Index wird erzeugt, welche Karte aus dem Deck gezogen wird
         var index: number = Math.floor( Math.random() * anzahlVerbleibenderKartenImStapel );
 
-        // Karte aus dem Kartendeck holen
+        // Karte aus dem Deck holen
         var karte: Karte = kartendeck[index];
 
-        // Karte aus dem Deck entfernen, damit diese nicht doppelt gezogen werden kann
+        // Die Karte wird aus dem Deck entfernt, damit die nicht doppelt gezogen wird.
         kartendeck.splice( index, 1 );
 
-        meineKarten.push( karte );
+        meineKarten.push( karte );  // Die Karte wird auf meinen Stapel gelegt
     }
 
-    function meineKartenZeichnen(): void {
+    function meineKartenZeichnen(): void { // Ohne Rückgaberecht
 
         for ( var i: number = 0; i < meineKarten.length; i++ ) {
             var karte: Karte = meineKarten[i];
 
-            //<div class="card"> 
+            //<div class="card">  und die Erzeugung von einem HTML Tags, damit die Karten angezeigt werden
             let card: HTMLDivElement = document.createElement( "div" );
             card.className = "card";
 
             // <div class="bg green"></div> 
             let bg: HTMLDivElement = document.createElement( "div" );
             bg.className = "bg " + karte.farbe;
-            card.appendChild( bg );
+            card.appendChild( bg ); // der Hintergrund dieser Karte wird hinzugefügt
 
-            // <div class="circle"></div> 
+            // <div class="circle"></div> und Ellipse wird ebenfalls hinzugefügt
             let kreis: HTMLDivElement = document.createElement( "div" );
             kreis.className = "circle";
-            card.appendChild( kreis );
+            card.appendChild( kreis ); // Hinzufügen der Ellipse für die Optik
 
             //  <div class="top-left">9</div> 
             let obenLinks: HTMLDivElement = document.createElement( "div" );
             obenLinks.className = "top-left";
-            obenLinks.innerHTML = karte.art;
-            card.appendChild( obenLinks );
+            obenLinks.innerHTML = karte.art; // Das Motiv der Karte, Zahl oder Zeichen
+            card.appendChild( obenLinks ); // Die Zahl wird oben links zur Karte hinzugefügt
 
             //  <div class="center">9</div> 
             let zentrum: HTMLDivElement = document.createElement( "div" );
             zentrum.className = "center";
-            zentrum.innerHTML = karte.art;
-            card.appendChild( zentrum );
+            zentrum.innerHTML = karte.art; // Das Hinzufügen der Zahl zentriert auf die Karte
+            card.appendChild( zentrum ); // Die Zahl wird zentriert und hinzugefügt
 
             // <div class="bottom-right">9</div> 
             let untenRechts: HTMLDivElement = document.createElement( "div" );
@@ -124,7 +128,7 @@ namespace uno {
             style.left = 10 + i * 100 + "px";
             style.bottom = "10px";
 
-            document.body.appendChild( card );
+            document.body.appendChild( card ); // Karte wird im HTML-Body hinzugefügt
         }
     }
 
